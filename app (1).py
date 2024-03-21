556ab166e1408f7389ee3cb7d7602bc2627c3d82
Bushra Akram <141483100+BushraAkram111@users.noreply.github.com>
2024-03-21 19:09:16 +0500

    Add files via upload
    Data_Visualizer and EDA Streamlit App is an intuitive and interactive web application crafted to streamline the exploratory data analysis (EDA) process. Built on the Streamlit framework, this tool empowers users to effortlessly upload datasets and delve into their data with ease. From examining descriptive statistics to visualizing distributions, correlations, and trends through a wide array of interactive charts and graphs, users can gain deep insights into their data with just a few clicks. With customizable features and real-time updates, Data_Visualizer and EDA Streamlit App offers a seamless experience for analysts, researchers, and data enthusiasts alike, enabling them to unlock valuable insights and drive informed decision-making.

---
+++ app (1).py
@@ -0,0 +1,153 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+import matplotlib.pyplot as plt 
+import matplotlib  
+matplotlib.use('Agg')
+import seaborn as sns 
+
+from sklearn import model_selection
+from sklearn.linear_model import  LogisticRegression
+from sklearn.neighbors import  KNeighborsClassifier    
+from sklearn.naive_bayes  import GaussianNB
+from sklearn.svm import SVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.tree import DecisionTreeClassifier
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+def main():
+
+    st.title('Data-Visualizer')
+    st.text('Built with Streamlit')
+    activities = ["EDA","Plot","Model Building","About"]
+
+    choice = st.sidebar.selectbox("Select Activity",activities)
+
+    if choice == 'EDA' : 
+        
+        st.subheader("Exploratory Data Analysis!!")
+        data = st.file_uploader("Upload Dataset",type = ["csv","txt","xls"])        
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+
+            if st.checkbox("Show shape"):
+                st.write(df.shape)
+
+            if st.checkbox("Show Columns"):
+                all_columns = df.columns.to_list()
+                st.write(all_columns)
+
+            if st.checkbox("Select Columns To Show"):
+                selected_columns = st.multiselect("Select Columns",all_columns)
+                new_df = df[selected_columns]
+                st.dataframe(new_df)
+
+            if st.checkbox("Show Summary"):
+                st.write(df.describe())
+
+            if st.checkbox("Show Value Counts"):
+                st.write(df.iloc[:,-1].value_counts()) # select the target columns..assuming last columns is target columns
+
+
+    elif choice == 'Plot' : 
+        st.subheader("Data Visualization")
+
+        data = st.file_uploader("Upload Dataset",type = ["csv","txt","xls"])        
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+
+        if st.checkbox("Correlation with Seaborn"):
+            # Check if the selected columns are numeric
+            numeric_columns = df.select_dtypes(include=[np.number]).columns.tolist()
+            if numeric_columns:
+                fig, ax = plt.subplots()
+                sns.heatmap(df[numeric_columns].corr(), annot=True, ax=ax)
+                st.pyplot(fig)
+            else:
+                st.write("No numeric columns to calculate correlation.")
+
+        if st.checkbox("Pie chart"):
+            all_columns = df.columns.to_list()
+            columns_to_plot = st.selectbox("Select Column",all_columns)
+            pie_plot = df[columns_to_plot].value_counts().plot.pie(autopct = "%1.1f%%")
+            st.write(pie_plot)
+            st.pyplot()
+
+        all_columns_names = df.columns.to_list()
+        type_of_plot = st.selectbox("Select type of Plot",["area","bar","line","hist","box","kde"])
+        selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+
+        if st.button("Generate Plot"):
+            st.success("Generating Customizable Plot {} for {}".format(type_of_plot,selected_columns_names))
+
+            if type_of_plot == 'area':
+                collect_data = df[selected_columns_names]
+                st.area_chart(collect_data)                
+            elif type_of_plot == 'bar':
+                collect_data = df[selected_columns_names]
+                st.bar_chart(collect_data)
+
+            elif type_of_plot == 'line':
+                collect_data = df[selected_columns_names]
+                st.line_chart(collect_data)
+
+            elif type_of_plot:
+                collect_data = df[selected_columns_names].plot(kind = type_of_plot)
+                st.write(collect_data)
+                st.pyplot()
+
+    elif choice == 'Model Building' : 
+        st.subheader("Building ML Model")
+        data = st.file_uploader("Upload Dataset",type = ["csv","txt","xls"])        
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+                
+            X = df.iloc[:,0:-1]
+            Y = df.iloc[:,-1]
+            seed = None  # Set random_state to None
+            
+            # Model Building : Assuming last column is target columns
+            models = []
+            models.append(("LR",LogisticRegression()))
+            models.append(("LDA",LinearDiscriminantAnalysis()))
+            models.append(("KNN",KNeighborsClassifier()))
+            models.append(("CART",DecisionTreeClassifier()))
+            models.append(("NB",GaussianNB()))
+            models.append(("SVM",SVC()))
+
+            # Evaluate each model in turn
+            model_names = []
+            model_mean = []
+            model_std = []
+            all_models = []
+            scoring = 'accuracy'
+
+            for name,model in models:
+                kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=seed)
+                cv_results = model_selection.cross_val_score(model,X,Y,cv = kfold,scoring=scoring)
+                model_names.append(name)
+                model_mean.append(cv_results.mean())
+                model_std.append(cv_results.std())
+
+                accuracy_results = {"model_name":name,"model_accuracy":cv_results.mean(),"standard_deviation":cv_results.std()}
+                all_models.append(accuracy_results)    
+
+            if st.checkbox("Metrics as Table"):
+                st.dataframe(pd.DataFrame(zip(model_names,model_mean,model_std),columns = ["Model","Accuracy","Standard_Deviation"]))
+
+            if st.checkbox("Metrics as JSON"):
+                st.json(all_models)
+
+    elif choice == 'About' : 
+        st.subheader("About")
+        st.markdown("""This project focuses at providing a more comprehensive way to interact with data.Created an Data
+        Visualization App using Streamlit library which works well with any data and help to Visualize
+        and extract relevant information from data in more easier way""")
+
+if __name__ == '__main__':
+    main()
